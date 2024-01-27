e6ccbecc70645f0c5b999152650129f164329d29
SimonYacobi <150596499+SimonYacobi@users.noreply.github.com>
2024-01-27 14:52:40 +0200

    done

--- SimonYacobiExe4/app/src/main/java/com/example/simonyacobiexe4/MainActivity.java
+++ SimonYacobiExe4/app/src/main/java/com/example/simonyacobiexe4/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.simonyacobiexe4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.btn);
+
+
+    }
+    public void go(View view) {
+        counter++;
+        updateButtonText();
+        if (counter % 7 == 0) {
+            btn.setText("BOOM!");
+
+        }
+    }
+    private void updateButtonText() {
+        btn.setText("This is a click number: " + counter);
     }
 }
