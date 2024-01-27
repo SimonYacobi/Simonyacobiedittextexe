99fe43d1d30c779bdecbc374e75808c2a68a9e8a
SimonYacobi <150596499+SimonYacobi@users.noreply.github.com>
2024-01-27 17:15:02 +0200

    start

---
+++ app/src/main/java/com/example/simonyacobiexe1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.simonyacobiexe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myfirstapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go(View view) {
+
+    }
+}
